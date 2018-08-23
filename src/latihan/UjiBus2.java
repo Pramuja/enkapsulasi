4688e0fd7320b4a08ae358e723b1c76f0a2a6e58
Pramuja <42104207+Pramuja@users.noreply.github.com>
2018-08-23 11:25:27 +0700

    Add files via upload

---
+++ src/latihan/UjiBus2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author USER
+ */
+public class UjiBus2 {
+    public static void main(String[] args) {
+        Bus2 Coba = new Bus2(50);
+        Coba.getPenumpang(27);
+        Coba.cetakpenumpang();
+        Coba.getAverage();
+        Coba.addPenumpang(2);
+        Coba.cetakpenumpang();
+        Coba.getAverage();
+        Coba.addPenumpang(5);
+        Coba.cetakpenumpang();
+        Coba.getAverage();
+        Coba.addPenumpang(10);
+        Coba.cetakpenumpang();
+        Coba.getAverage();
+        Coba.addPenumpang(3);
+        Coba.cetakpenumpang();
+        Coba.getAverage();
+    }
+}
